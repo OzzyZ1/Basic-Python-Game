7eaa3dc94c61aa62c0a1c1f9f623c42577f91326
OzzyZ1 <126298104+OzzyZ1@users.noreply.github.com>
2023-02-25 00:45:28 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,123 +1,100 @@
 import pygame
 import random
-import sys
 
+# Define the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Set the width and height of the screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Set the speed of the player's car
+PLAYER_SPEED = 5
+
+# Initialize Pygame
 pygame.init()
 
-WIDTH = 800
-HEIGHT = 600
+# Set the size of the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-RED = (255,0,0)
-BLUE = (0,0,255)
-YELLOW = (255,255,0)
-BACKGROUND_COLOR = (0,0,0)
+# Set the caption of the game window
+pygame.display.set_caption("Racing Game")
 
-player_size = 50
-player_pos = [WIDTH/2, HEIGHT-2*player_size]
+# Load the images
+player_car = pygame.image.load("player_car.png")
+other_car = pygame.image.load("other_car.png")
+coin = pygame.image.load("coin.png")
+oil_slick = pygame.image.load("oil_slick.png")
+roadblock = pygame.image.load("roadblock.png")
 
-enemy_size = 50
-enemy_pos = [random.randint(0,WIDTH-enemy_size), 0]
-enemy_list = [enemy_pos]
+# Create the player's car
+player_x = SCREEN_WIDTH / 2 - player_car.get_width() / 2
+player_y = SCREEN_HEIGHT - player_car.get_height() - 50
+player_rect = pygame.Rect(player_x, player_y, player_car.get_width(), player_car.get_height())
 
-SPEED = 10
+# Create the list of other cars
+other_cars = []
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# Create the list of coins
+coins = []
 
-game_over = False
+# Create the list of obstacles
+obstacles = []
 
+# Create the font for the score
+font = pygame.font.SysFont(None, 30)
+
+# Set the starting score
 score = 0
 
+# Set the starting time
+time = 60
+
+# Set the level
+level = 1
+
+# Set the game state
+game_over = False
+
+# Set the clock
 clock = pygame.time.Clock()
 
-myFont = pygame.font.SysFont("monospace", 35)
-
-def set_level(score, SPEED):
-	if score < 20:
-		SPEED = 5
-	elif score < 40:
-		SPEED = 8
-	elif score < 60:
-		SPEED = 12
-	else:
-		SPEED = 15
-	return SPEED
-	# SPEED = score/5 + 1
-
-
-def drop_enemies(enemy_list):
-	delay = random.random()
-	if len(enemy_list) < 10 and delay < 0.1:
-		x_pos = random.randint(0,WIDTH-enemy_size)
-		y_pos = 0
-		enemy_list.append([x_pos, y_pos])
-
-def draw_enemies(enemy_list):
-	for enemy_pos in enemy_list:
-		pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
-
-def update_enemy_positions(enemy_list, score):
-	for idx, enemy_pos in enumerate(enemy_list):
-		if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
-			enemy_pos[1] += SPEED
-		else:
-			enemy_list.pop(idx)
-			score += 1
-	return score
-
-def collision_check(enemy_list, player_pos):
-	for enemy_pos in enemy_list:
-		if detect_collision(enemy_pos, player_pos):
-			return True
-	return False
-
-def detect_collision(player_pos, enemy_pos):
-	p_x = player_pos[0]
-	p_y = player_pos[1]
-
-	e_x = enemy_pos[0]
-	e_y = enemy_pos[1]
-
-	if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x+enemy_size)):
-		if (e_y >= p_y and e_y < (p_y + player_size)) or (p_y >= e_y and p_y < (e_y+enemy_size)):
-			return True
-	return False
-
+# The game loop
 while not game_over:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
+    # Move the player's car
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player_x > 0:
+        player_x -= PLAYER_SPEED
+    if keys[pygame.K_RIGHT] and player_x < SCREEN_WIDTH - player_car.get_width():
+        player_x += PLAYER_SPEED
 
-		if event.type == pygame.KEYDOWN:
+    # Spawn other cars
+    if random.randint(0, 100) < level:
+        other_car_x = random.randint(0, SCREEN_WIDTH - other_car.get_width())
+        other_car_y = -other_car.get_height()
+        other_car_rect = pygame.Rect(other_car_x, other_car_y, other_car.get_width(), other_car.get_height())
+        other_cars.append(other_car_rect)
 
-			x = player_pos[0]
-			y = player_pos[1]
+    # Spawn coins
+    if random.randint(0, 1000) < level:
+        coin_x = random.randint(0, SCREEN_WIDTH - coin.get_width())
+        coin_y = -coin.get_height()
+        coin_rect = pygame.Rect(coin_x, coin_y, coin.get_width(), coin.get_height())
+        coins.append(coin_rect)
 
-			if event.key == pygame.K_LEFT:
-				x -= player_size
-			elif event.key == pygame.K_RIGHT:
-				x += player_size
+    # Spawn obstacles
+    if random.randint(0, 100) < level:
+        obstacle_x = random.randint(0, SCREEN_WIDTH - roadblock.get_width())
+        obstacle_y = -roadblock.get_height()
+        obstacle_rect = pygame.Rect(obstacle_x, obstacle_y, roadblock
 
-			player_pos = [x,y]
-
-	screen.fill(BACKGROUND_COLOR)
-
-	drop_enemies(enemy_list)
-	score = update_enemy_positions(enemy_list, score)
-	SPEED = set_level(score, SPEED)
-
-	text = "Score:" + str(score)
-	label = myFont.render(text, 1, YELLOW)
-	screen.blit(label, (WIDTH-200, HEIGHT-40))
-
-	if collision_check(enemy_list, player_pos):
-		game_over = True
-		break
-
-	draw_enemies(enemy_list)
-
-	pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
-
-	clock.tick(30)
-
-	pygame.display.update()
+				    
